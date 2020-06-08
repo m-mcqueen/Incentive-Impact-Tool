22567b34ae76a797bb0091cb6a3104db1d3ad841
futurDesign <mike.mcqueen@pdx.edu>
2020-06-08 12:14:18 -0700

    Made a small script to save the .csv data tables as .RData format for better server efficiency.

---
+++ E-Bike_Incentive_Design_Space/www/Data Tables/process_data_for_web_use.R
@@ -0,0 +1,20 @@
+#Process Data for Web Use
+#Save data as native R format for more efficient server operations
+#Mike McQueen
+
+#Load data to save as .RDS
+#State Info
+Electricity_raw <- read.csv("www/Data Tables/State Info/Electricity.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+Incentives_raw <- read.csv("www/Data Tables/State Info/Incentives.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+Trips_raw <- read.csv("www/Data Tables/State Info/Trips.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+
+#Vehicles
+BEV_raw <- read.csv("www/Data Tables/Vehicles/BEV.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+PHEV_raw <- read.csv("www/Data Tables/Vehicles/PHEV.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+FCEV_raw <- read.csv("www/Data Tables/Vehicles/FCEV.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+ICE_raw <- read.csv("www/Data Tables/Vehicles/ICE.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+EBike_raw <- read.csv("www/Data Tables/Vehicles/EBike.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE)
+mix_raw <- read.csv("www/Data Tables/Vehicles/mix.csv", fileEncoding="UTF-8-BOM", stringsAsFactors = FALSE) #Defines a specific combo of vehicle types
+
+#export as .RDS
+save(Electricity_raw, Incentives_raw, Trips_raw, BEV_raw, PHEV_raw, FCEV_raw, ICE_raw, EBike_raw, mix_raw, file = "www/Data Tables/data.RData")
